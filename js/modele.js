de0b0da3ed102244d17fc6fbb0a56e034f592e5e
giusmili <giusmili67@gmail.com>
2025-01-31 15:19:35 +0100

    js import

---
+++ js/modele.js
@@ -0,0 +1,15 @@
+export const elements = {
+    elParent: document.querySelector("ul"),
+    elImg: document.querySelector("figure img")
+
+    /* Objet js */
+};export const user = {
+    cover: "./asset/ai-generated-9241538_1280.jpg",
+    Langage: "JScript, JScript.NET, Objective-J, TIScript",
+    Auteur: "Brendan Eich",
+    Date: "Mai 1996",
+    version: "é13 - ECMA-262 (Juin 2022)",
+    Developer: "Netscape Communications Corporation, Mozilla Foundation",
+    Extension: "js et mjs",
+    Influence: "AWK, Self, Scheme, Perl, C, C++, Java, Python"
+};
