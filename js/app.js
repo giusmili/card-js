55b109728f794561d296000a799a456238d990f0
giusmili <giusmili67@gmail.com>
2025-01-31 14:48:37 +0100

    js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", e => {
+    e.preventDefault();
+
+    console.log("chargement du document completée");
+
+    /* Objet js */
+    const user = {
+        cover: "./asset/ai-generated-9241538_1280.jpg",
+        Langage: "JScript, JScript.NET, Objective-J, TIScript",
+        Auteur: "Brendan Eich",
+        Date: "Mai 1996",
+        version: é13 - "ECMA-262 (Juin 2022)",
+        Developer: "Netscape Communications Corporation, Mozilla Foundation",
+        Extension: "js et mjs",
+        Influence: "AWK, Self, Scheme, Perl, C, C++, Java, Python"
+    };
+});
